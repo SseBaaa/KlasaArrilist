3163ae6b72e7c2fd542bf342bb2ecde24efe65f4
sebastian krnjus <sebastiankrnjus25@gmail.com>
2022-09-19 12:18:07 +0200

    Add project files.

---
+++ KlasaArrilist/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace KlasaArrilist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNovi = new System.Windows.Forms.Button();
+            this.buttonUnesi = new System.Windows.Forms.Button();
+            this.textBoxNovi = new System.Windows.Forms.TextBox();
+            this.textBoxLista = new System.Windows.Forms.TextBox();
+            this.buttonObrisi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonNovi
+            // 
+            this.buttonNovi.Location = new System.Drawing.Point(51, 35);
+            this.buttonNovi.Name = "buttonNovi";
+            this.buttonNovi.Size = new System.Drawing.Size(225, 27);
+            this.buttonNovi.TabIndex = 0;
+            this.buttonNovi.Text = "Novi Automobil";
+            this.buttonNovi.UseVisualStyleBackColor = true;
+            this.buttonNovi.Click += new System.EventHandler(this.buttonNovi_Click);
+            // 
+            // buttonUnesi
+            // 
+            this.buttonUnesi.Enabled = false;
+            this.buttonUnesi.Location = new System.Drawing.Point(289, 96);
+            this.buttonUnesi.Name = "buttonUnesi";
+            this.buttonUnesi.Size = new System.Drawing.Size(75, 23);
+            this.buttonUnesi.TabIndex = 1;
+            this.buttonUnesi.Text = "Unesi";
+            this.buttonUnesi.UseVisualStyleBackColor = true;
+            this.buttonUnesi.Click += new System.EventHandler(this.buttonUnesi_Click);
+            // 
+            // textBoxNovi
+            // 
+            this.textBoxNovi.Enabled = false;
+            this.textBoxNovi.Location = new System.Drawing.Point(51, 96);
+            this.textBoxNovi.Name = "textBoxNovi";
+            this.textBoxNovi.Size = new System.Drawing.Size(225, 20);
+            this.textBoxNovi.TabIndex = 2;
+            // 
+            // textBoxLista
+            // 
+            this.textBoxLista.Location = new System.Drawing.Point(51, 145);
+            this.textBoxLista.Multiline = true;
+            this.textBoxLista.Name = "textBoxLista";
+            this.textBoxLista.Size = new System.Drawing.Size(225, 139);
+            this.textBoxLista.TabIndex = 3;
+            // 
+            // buttonObrisi
+            // 
+            this.buttonObrisi.Location = new System.Drawing.Point(51, 312);
+            this.buttonObrisi.Name = "buttonObrisi";
+            this.buttonObrisi.Size = new System.Drawing.Size(75, 23);
+            this.buttonObrisi.TabIndex = 4;
+            this.buttonObrisi.Text = "Obrisi listu";
+            this.buttonObrisi.UseVisualStyleBackColor = true;
+            this.buttonObrisi.Click += new System.EventHandler(this.buttonObrisi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 358);
+            this.Controls.Add(this.buttonObrisi);
+            this.Controls.Add(this.textBoxLista);
+            this.Controls.Add(this.textBoxNovi);
+            this.Controls.Add(this.buttonUnesi);
+            this.Controls.Add(this.buttonNovi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonNovi;
+        private System.Windows.Forms.Button buttonUnesi;
+        private System.Windows.Forms.TextBox textBoxNovi;
+        private System.Windows.Forms.TextBox textBoxLista;
+        private System.Windows.Forms.Button buttonObrisi;
+    }
+}
+
---
+++ KlasaArrilist/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace KlasaArrilist
+{
+    public partial class Form1 : Form
+    {
+        ArrayList listaAutomobila = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNovi_Click(object sender, EventArgs e)
+        {
+            textBoxNovi.Enabled = true;
+            buttonUnesi.Enabled = true;
+        }
+
+        private void buttonUnesi_Click(object sender, EventArgs e)
+        {
+            textBoxNovi.Enabled = false;
+            buttonUnesi.Enabled = false;
+            //textBoxLista.AppendText(textBoxNovi.Text + Environment.NewLine);
+            Automobil a = new Automobil(textBoxNovi.Text);
+            listaAutomobila.Add(a);
+            textBoxLista.AppendText(listaAutomobila.IndexOf(a) + 1 + ". " + a.dajMarku() + Environment.NewLine);
+            textBoxNovi.Clear();
+        }
+
+        private void buttonObrisi_Click(object sender, EventArgs e)
+        {
+            textBoxLista.Clear();
+        }
+    }
+}
